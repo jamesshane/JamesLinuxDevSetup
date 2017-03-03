3dbb69540696c78a2f8a0c5d62d17f94f6fc1d8f
james <james@kitt.(none)>
2017-03-03 00:23:34 -0700

    Added zsh check

--- MakeItReset.sh
+++ MakeItReset.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+pat=`echo $SHELL`
+if  [[ $pat == *zsh  ]]  ;
+	then
+		echo "Zsh will be removed, might want to switch to bash: chsh -s /bin/bash" 
+		exit 1
+fi
+echo "Let's Go!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 sudo dpkg --clear-selections
 sudo dpkg --set-selections < list.txt
 sudo apt-get autoremove
